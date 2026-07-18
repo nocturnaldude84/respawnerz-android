c9a6b99d3b8d5a761ceb24090f6686e1829e8dfb
nocturnaldude84 <53819942+nocturnaldude84@users.noreply.github.com>
2026-07-18 18:54:36 +0530

    v1.3 final

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.app.respawnerz"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.2"
+        versionCode = 3
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
